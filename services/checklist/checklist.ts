008c81ae4cdf9760d3edf2ee8cc9cf32edc9465f
Rodrigo Mello <rodrigojcmello@gmail.com>
2023-04-21 20:22:37 -0300

    chore: code optimization

--- services/checklist/checklist.ts
+++ services/checklist/checklist.ts
@@ -2,34 +2,19 @@ import axios from 'axios';
 import type { CheckList, Farmer } from '@/services/checklist/types';
 
 const api = axios.create({
-  baseURL: 'http://challenge-front-end.bovcontrol.com/v1',
+  baseURL: 'http://challenge-front-end.bovcontrol.com',
 });
 
 export const getChecklist = async (): Promise<CheckList> => {
-  try {
-    const response = await api.get('/checkList');
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  const response = await api.get('/v1/checkList');
+  return response.data;
 };
 
 export const getChecklistID = async (id?: string): Promise<Farmer> => {
-  try {
-    const response = await api.get(`/checkList/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  const response = await api.get(`/v1/checkList/${id}`);
+  return response.data;
 };
 
 export const setChecklist = async (data: Farmer[]): Promise<void> => {
-  try {
-    await api.post('/checkList', { checklists: data });
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  await api.post('/v1/checkList', { checklists: data });
 };
