8914dfea38970b47236de4ff07a94f87ea350299
Rodrigo Mello <rodrigojcmello@gmail.com>
2023-04-23 12:22:21 -0300

    feat: backButton go back based in history

--- components/BackButton/BackButton.tsx
+++ components/BackButton/BackButton.tsx
@@ -1,13 +1,23 @@
-import type { FC } from 'react';
+import type { FC, MouseEvent } from 'react';
 import Link from 'next/link';
 import { BackIcon } from '@/components/Icons';
 import { BackButtonStyle } from '@/components/BackButton/BackButton.style';
 import type { BackButtonProps } from '@/components/BackButton/BackButton.types';
+import { useRouter } from 'next/router';
 
 export const BackButton: FC<BackButtonProps> = ({ href }) => {
+  const { back } = useRouter();
+
+  const clickBack = (event?: MouseEvent<HTMLAnchorElement>) => {
+    if (!href) {
+      event?.preventDefault();
+      back();
+    }
+  };
+
   return (
     <BackButtonStyle>
-      <Link className={'back_button'} href={href || `/`}>
+      <Link className={'back_button'} onClick={clickBack} href={href || `/`}>
         <BackIcon />
       </Link>
     </BackButtonStyle>
