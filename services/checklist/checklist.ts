09686f417595fe0b297325561aa3c91a41497d72
Rodrigo Mello <rodrigojcmello@gmail.com>
2023-04-23 17:51:03 -0300

    chore: review requests

--- services/checklist/checklist.ts
+++ services/checklist/checklist.ts
@@ -3,36 +3,31 @@ import type { CheckList, Farmer } from '@/services/checklist';
 
 const api = axios.create({
   baseURL: 'http://challenge-front-end.bovcontrol.com',
+  timeout: 3000,
 });
 
 export const getAllFarms = async (): Promise<CheckList> => {
-  // TODO: handle errors
   const response = await api.get('/v1/checkList');
   return response.data;
 };
 
 export const getOneFarmByID = async (id?: string): Promise<Farmer> => {
-  // TODO: handle errors
   const response = await api.get(`/v1/checkList/${id}`);
   return response.data;
 };
 
 export const setNewFarm = async (data: Farmer[]): Promise<void> => {
-  // TODO: handle errors
   await api.post('/v1/checkList', { checklists: data });
 };
 
 export const deleteFarmByID = async (id: string): Promise<void> => {
-  // TODO: handle errors
-  const response = await api.delete(`/v1/checkList/${id}`);
-  return response.data;
+  await api.delete(`/v1/checkList/${id}`);
 };
 
 export const updateFarmByID = async (
   id: string,
   data: Farmer
 ): Promise<Farmer> => {
-  // TODO: handle errors
   const response = await api.put(`/v1/checkList/${id}`, data);
   return response.data;
 };
